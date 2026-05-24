6126de4acf1a4d112251d55ca8344694a99b610b
Swayam <swayamjain0905@gmail.com>
2026-05-24 17:42:20 +0530

    Active Page Link Highlights

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const global = {
+  currPage: window.location.pathname,
+};
+
+// Highlight active link
+function highlightActiveLink() {
+  const links = document.querySelectorAll('.nav-link');
+  links.forEach((link) => {
+    if (link.getAttribute('href') === global.currPage) {
+      link.classList.add('active');
+    }
+  });
+}
+
+// Init App
+function init() {
+  switch (global.currPage) {
+    case '/':
+    case '/index.html':
+      console.log('Home');
+      break;
+    case '/shows.html':
+      console.log('Shows');
+      break;
+    case '/movie-details.html':
+      console.log('Movie Details');
+      break;
+    case '/tv-details.html':
+      console.log('Show Details');
+      break;
+    case '/search.html':
+      console.log('Search');
+      break;
+  }
+
+  highlightActiveLink();
+}
+
+document.addEventListener('DOMContentLoaded', init);
